b5df9bcb3920d7bc9674a73fb719452f94f16c2e
niloynkj <66770175+niloynkj@users.noreply.github.com>
2022-04-03 22:27:36 +0600

    Create ecom.js

---
+++ ecom.js
@@ -0,0 +1,44 @@
+
+
+    var MenuItems = document.getElementById("MenuItems");
+
+    MenuItems.style.maxHeight = "0px";
+
+    function menutoggle(){
+
+if(MenuItems.style.maxHeight == "0px"){
+    MenuItems.style.maxHeight = "200px";
+
+}
+
+else{
+
+
+    MenuItems.style.maxHeight = "0px";
+}
+
+    }
+
+    </script>
+</script>
+
+<!-------Js for Product Gallery-->
+<script>
+    var productImg = document.getElementById("productImg");
+    var smallImg = document.getElementsByClassName("small-img");
+    smallImg[0].onclick = function()
+    {
+        productImg.src = smallImg[0].src;
+    }
+    smallImg[1].onclick = function()
+    {
+        productImg.src = smallImg[1].src;
+    }
+    smallImg[2].onclick = function()
+    {
+        productImg.src = smallImg[2].src;
+    }
+    smallImg[3].onclick = function()
+    {
+        productImg.src = smallImg[3].src;
+    }
